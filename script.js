039bfb1c0cd526331c4ebdbcf618492a90661305
KhalidO121 <khalidokewole383@gmail.com>
2022-02-26 13:56:46 +0000

    Adding project files to a repository.
    Adding the files from my project to a repository.

---
+++ script.js
@@ -0,0 +1,52 @@
+// import SWUP from 'swup'
+
+let intro = document.querySelector('.intro')
+let logo = document.querySelector('.logo-header')
+let logoSpan = document.querySelectorAll('.logo')
+
+window.addEventListener('DOMContentLoaded', () =>{
+
+    setTimeout(()=>{
+
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            },(idx + 1) * 400)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+
+                }, (idx + i) * 50)
+            })
+        },2000);
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        },4300);
+    })
+})
+
+// Pressing play to start the game.
+function startGame(){
+    let localButton = document.getElementById("1V1 MODE")
+    let displayButton = localButton.style.display
+
+    if(displayButton == 'block'){
+        localButton.style.display = 'none'
+
+        document.getElementById("1V1 MODE").innerHTML = "Let's Not Play!"
+    }
+
+    else{
+        localButton.style.display = 'block'
+
+        document.getElementById("1V1 MODE").innerHTML = "Let's Play!"
+
+    }
+    // document.getElementById("1V1 MODE").innerHTML = "Let's Play!"
+}
