d2a8123aa5cf2c69b5862233a88a638c165c2956
KhalidO121 <khalidokewole383@gmail.com>
2022-03-24 13:31:54 +0000

    Adding multiplayer functionality
    When user reaches play menu it connects them to the server(node js), and then message is displayed in terminal to indicate user has joined.

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const Websocket = require("ws");
+
+const WebsocketServer = new Websocket.Server({ port:8050 });
+
+WebsocketServer.on("connection", client =>{
+    console.log("New client has connected!");
+
+    client.on("close", ()=>{
+            console.log("Client has disconnected!")
+        });
+});
