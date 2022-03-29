113fee77f2110c77e26bee81b42392651afde471
KhalidO121 <khalidokewole383@gmail.com>
2022-03-29 12:14:10 +0100

    Multiplayer

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const WebsocketServer = new Websocket.Server({ port:8050 });
 WebsocketServer.on("connection", client =>{
     console.log("New client has connected!");
 
+    client.send("Hello".toLowerCase());
+
     client.on("close", ()=>{
             console.log("Client has disconnected!")
         });
